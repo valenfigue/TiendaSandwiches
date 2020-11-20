ff1f74879693e668252e13e9b24b26b65daccf82
Valentina Figueroa <valenfigue14@gmail.com>
2020-11-20 10:33:01 -0400

    Agregados archivos previos a la creación del ejecutable.

---
+++ setup.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup 
+import py2exe 
+ 
+setup(name="main", 
+ version="1.0", 
+ description="Mini proyecto 1 de Programación con Python", 
+ author="Valentina Figueroa", 
+ author_email="valenfigue14@gmail.com", 
+ url="", 
+ license="", 
+ scripts=["main.py, 
+ console=["main.py, 
+ options={"py2exe": {"bundle_files": 1}}, 
+ zipfile=None,
+)
